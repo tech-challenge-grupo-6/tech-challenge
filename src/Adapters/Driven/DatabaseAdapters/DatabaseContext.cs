d85cc23029138c824123ab69f469f94c4318a389
Vitor de Souza <vitordesolza@gmail.com>
2023-10-01 14:41:45 -0300

    Adicionando suporte ao banco mysql

---
+++ src/Adapters/Driven/DatabaseAdapters/DatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseAdapters;
+
+public class DatabaseContext : DbContext
+{
+    public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+    {
+
+    }
+
+    override protected void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(DatabaseContext).Assembly);
+    }
+}
