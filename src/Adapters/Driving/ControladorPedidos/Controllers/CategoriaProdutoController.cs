059e0bf6947650cfd2ab39fb8ca1781ae5bba3a1
victorhiga2018 <viih.higa@gmail.com>
2023-10-06 01:24:20 -0300

    feat: implementa escopo da controller para cada entidade

---
+++ src/Adapters/Driving/ControladorPedidos/Controllers/CategoriaProdutoController.cs
@@ -0,0 +1,40 @@
+using DatabaseAdapters;
+using DatabaseAdapters.Repositories;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControladorPedidos.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CategoriaProdutoController : ControllerBase
+{
+
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        return new string[] { "value1", "value2" };
+    }
+
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+        return "value";
+    }
+
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+    }
+
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+}
