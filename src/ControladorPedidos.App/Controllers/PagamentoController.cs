f2cef400f9f7ca731dc44770fdafe9188c08bdb8
Renan Silva Xavier <renansxdev@gmail.com>
2023-12-21 20:49:48 -0300

    Feat/consultar status pagamento (#96)
    * feat: adicionado nova rota de consultar pagamento a partir do id do pedido, adicionado novos metodos nos repository, controler e usecase
    
    * retorno mensagem de pagamento aprovado ou não
    
    * Update GetByPedidoId method return type
    
    ---------
    
    Co-authored-by: Vitor de Souza <vitordesolza@gmail.com>

--- src/ControladorPedidos.App/Controllers/PagamentoController.cs
+++ src/ControladorPedidos.App/Controllers/PagamentoController.cs
@@ -1,4 +1,5 @@
 ﻿using ControladorPedidos.App.Contracts;
+using ControladorPedidos.App.Entities.Exceptions;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ControladorPedidos.App.Controllers;
@@ -28,6 +29,40 @@ public class PagamentoController(IPagamentoUseCase pagamentoUseCase, ILogger<Pag
         {
             logger.LogError(ex, "Erro ao efetuar pagamento do pedido {PedidoId}", pedidoId);
             return BadRequest($"Erro ao efetuar pagamento do pedido {pedidoId}");
+        }
+    }
+    /// <summary>
+    /// Verifica o status do pagamento do pedido
+    /// </summary>
+    /// <param name="pedidoId">Id do pedido</param>
+    /// <response code="200">Retorna se pagamento foi aprovado ou não.</response>
+    /// <response code="400">Bad request.</response>
+    /// <response code="404">Não encontrado.</response>
+    /// <response code="500">Erro interno.</response>
+    [HttpGet("check/{pedidoId}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> Get(Guid pedidoId)
+    {
+        try
+        {
+            logger.LogInformation("Consultando status pagamento do pedido {PedidoId}", pedidoId);
+            var pagamento = await pagamentoUseCase.ConsultarPagamentoPeloPedido(pedidoId);
+            var successResponse = new { status = "Aprovado" };
+            return Ok(successResponse);
+        }
+        catch (NotFoundException e)
+        {
+            logger.LogError(e.Message);
+            var errorResponse = new { status = "Não Aprovado" };
+            return NotFound(errorResponse);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Erro ao tentar consultar o status de pagamento do pedido {PedidoId}", pedidoId);
+            return BadRequest($"Erro ao tentar consultar o status de pagamento do pedido {pedidoId}");
         }
     }
 }
--- src/ControladorPedidos.App/Gateways/PagamentoRepository.cs
+++ src/ControladorPedidos.App/Gateways/PagamentoRepository.cs
@@ -1,6 +1,7 @@
 ﻿using ControladorPedidos.App.Entities;
 using ControladorPedidos.App.Entities.Repositories;
 using ControladorPedidos.App.Infrastructure.DataBase;
+using Microsoft.EntityFrameworkCore;
 
 namespace ControladorPedidos.App.Gateways;
 
@@ -10,5 +11,10 @@ public class PagamentoRepository(DatabaseContext dbContext) : IPagamentoReposito
     {
         dbContext.Pagamentos.Add(pagamento);
         await dbContext.SaveChangesAsync();
+    }
+
+    public async Task<Pagamento?> GetByPedidoId(Guid pedidoId)
+    {
+        return await dbContext.Pagamentos.FirstOrDefaultAsync(p => p.PedidoId == pedidoId);
     }
 }
